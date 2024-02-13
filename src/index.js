68fcb62215f803fc8b87ec4aafe924cc96dafcca
jinghuang <jinghuang0628@gmail.com>
2024-02-13 14:28:33 +0100

    Co-authored-by: Trista Shan <trista1987@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 console.log("Start Here 😉");
+const darkColor = document.getElementById("dark").checked;
+const lightColor = document.getElementById("light")
+const crazyColor = document.getElementById("crazy")
+const darkColorTest = document.getElementById("boxColor")
+const radios = document.querySelectorAll('input[type="radio"]');
+//darkColor.style.background = "brown"
+
+//box.style.backgroundColor = "grey";
+
+/*if(darkColor.checked){
+    darkColorTest.style.backgroundColor = "brown"
+}*/
+
+function changeColor (){
+    if (darkColor){
+        box.style.backgroundColor = "brown";
+    }else if(lightColor){
+        box.style.backgroundColor = "yellow";
+    }else if (crazyColor){
+        box.style.backgroundColor = "purple";
+    }
+}
+
+changeColor()
+
+/* radios.addEventListener("click", changeColor = () => {
+    if (darkColor){
+        box.style.backgroundColor = "brown"
+    }else if(lightColor){
+        box.style.backgroundColor = "yellow"
+    }else if (crazyColor){
+        box.style.backgroundColor = "purple"
+    }
+}) */
